61419b45090f1253fb2217662185dea26e691a02
VitorPhantomhive <vitorkhaled47@gmail.com>
2022-08-14 17:48:57 -0300

    morte com animação

--- JS/script.js
+++ JS/script.js
@@ -11,11 +11,26 @@ const jump = () =>{
 
 const loop = setInterval(() => {
 
-const pipePosition = pipe.offsetLeft;
+    console.log('loop');
 
-if (pipePosition < 120){
+const pipePosition = pipe.offsetLeft;
+const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
+
+
+if (pipePosition < 120 && pipePosition > 0 && marioPosition < 80){
     pipe.style.animation = 'none';
     pipe.style.left = `${pipePosition}px`; 
+
+    mario.style.animation = 'none';
+    mario.style.bottom = `${marioPosition}px`; 
+
+    mario.src = './images/game-over.png';
+    mario.style.width = '75px';
+    mario.style.marginLeft = '50px';
+
+
+    clearInterval(loop);
+    
 }
 
 }, 10)
